6857fa82f23f3cdc81596713d15c5767b81ee575
kiimmi <90731265+kiimmi@users.noreply.github.com>
2022-03-17 10:55:27 -0400

    g

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,10 @@
-module.exports = function {eleventyConfig} {
-    eleventyConfig.addPassthroughCopy{"scr/style"}
-    eleventyConfig.addPassthroughCopy{"scr/style"}
-    eleventyConfig.addPassthroughCopy{"scr/style"}
-    return{
-        dir:{
-            input: "scr",
-            output: "dist",
-        },
-    };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy9("styles");
+
+  return {
+    dir: {
+      input: "scr",
+      output: "dist",
+    },
+  };
 };
