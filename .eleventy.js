6eae4b7292014c1a3e04737daccdf46028900b80
kiimmi <90731265+kiimmi@users.noreply.github.com>
2022-03-24 08:46:48 -0400

    ?

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy9("styles");
+  eleventyConfig.addPassthroughCopy9("scr/styles");
+  eleventyConfig.addPassthroughCopy9("scr/md");
+  eleventyConfig.addPassthroughCopy9("scr/media");
 
   return {
     dir: {
